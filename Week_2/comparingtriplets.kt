167fc504c77f93cc92dda94675367b5aa5de7f6b
daisy2111-cheneno <87646709+daisy2111-cheneno@users.noreply.github.com>
2022-06-26 11:18:54 +0300

    fifth commit

---
+++ Week_2/comparingtriplets.kt
@@ -0,0 +1,55 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    
+    var aAward =0 
+    var bAward= 0 
+    // Write your code here
+    
+    for (i in 0..2){
+        if(a[i]>b[i]){
+            aAward += 1
+        }
+        if(a[i]<b[i]){
+            bAward +=1
+        } 
+    }
+return arrayOf(aAward,bAward);
+}
+
+fun main(args: Array<String>) {
+
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = compareTriplets(a, b)
+
+    println(result.joinToString(" "))
+}
